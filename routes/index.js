346b326d583ffd826e70739fcfbbbe900f2f4392
lugvictoria <victory.senty@yandex.ru>
2023-07-08 20:36:25 +0300

    controller for invalid url

--- routes/index.js
+++ routes/index.js
@@ -1,19 +1,21 @@
 const express = require('express');
+// const { celebrate, Joi } = require('celebrate');
+
 const { movies } = require('./movies');
 const { users } = require('./users');
-// const { celebrate, Joi } = require('celebrate');
+const { NotFoundError } = require('../errors');
 
 const routes = express.Router();
 
 routes.all('*', express.json());
 // routes.post('/signup', );
-
 // routes.post('/signin', );
-
 routes.use('/users', users);
 
 routes.use('/movies', movies);
 
-// routes.all('*')
+routes.all('*', (req, res, next) => {
+  next(new NotFoundError('Неверный адрес запроса'));
+});
 
 module.exports = { routes };
