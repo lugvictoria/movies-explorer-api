fe0d6253edcd4068d325137a4f78c7c7bd904fd9
lugvictoria <victory.senty@yandex.ru>
2023-07-08 20:34:42 +0300

    добавить error handler to app.js

--- app.js
+++ app.js
@@ -4,14 +4,13 @@ const mongoose = require('mongoose');
 
 // modules
 const { routes } = require('./routes');
+const { errorHandler } = require('./middlewares/errorHandler');
 
 // params
 const { PORT = 3000 } = process.env;
 const DATABASE_URL = 'mongodb://127.0.0.1:27017/bitfilmsdb';
-
 // initializing
 const app = express();
-
 mongoose
   .connect(DATABASE_URL)
   .then(() => {
@@ -21,15 +20,13 @@ mongoose
     console.log('Error on database connection');
     console.error(err);
   });
-
 // middlewares
 app.use(routes);
 
 // error handlers
+app.use(errorHandler);
 
 // app starting
 app.listen(PORT, () => {
   console.log(`App started on port ${PORT}...`);
 });
-n
-n
