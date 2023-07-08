ed55aad426a480c45f1b10e92df2c8746fc57383
lugvictoria <victory.senty@yandex.ru>
2023-07-08 20:23:53 +0300

    базовая структура для роутов

--- routes/index.js
+++ routes/index.js
@@ -1,17 +1,18 @@
 const express = require('express');
-const { celebrate, Joi } = require('celebrate');
+const { movies } = require('./movies');
+const { users } = require('./users');
+// const { celebrate, Joi } = require('celebrate');
 
 const routes = express.Router();
 
 routes.all('*', express.json());
-
 // routes.post('/signup', );
 
 // routes.post('/signin', );
 
-// routes.use('/users', auth, users);
+routes.use('/users', users);
 
-// routes.use('/movies', auth, movies);
+routes.use('/movies', movies);
 
 // routes.all('*')
 
