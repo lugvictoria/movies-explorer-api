d77ea05100e9e07b548a8d7bd0aeb3b7a8b08c0c
lugvictoria <victory.senty@yandex.ru>
2023-07-08 20:52:41 +0300

    authorization control middleware

--- routes/index.js
+++ routes/index.js
@@ -1,21 +1,22 @@
 const express = require('express');
 // const { celebrate, Joi } = require('celebrate');
-
 const { movies } = require('./movies');
 const { users } = require('./users');
 const { NotFoundError } = require('../errors');
+const { auth } = require('../middlewares/auth');
 
 const routes = express.Router();
 
 routes.all('*', express.json());
 // routes.post('/signup', );
-// routes.post('/signin', );
-routes.use('/users', users);
 
-routes.use('/movies', movies);
+// routes.post('/signin', );
+
+routes.use('/users', auth, users);
+
+routes.use('/movies', auth, movies);
 
 routes.all('*', (req, res, next) => {
   next(new NotFoundError('Неверный адрес запроса'));
 });
-
 module.exports = { routes };
