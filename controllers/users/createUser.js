4b1d1acd32ea48159b153f0aa0831dacd848d87c
lugvictoria <victory.senty@yandex.ru>
2023-07-09 14:45:13 +0300

    move app params to .env

--- controllers/users/createUser.js
+++ controllers/users/createUser.js
@@ -1,20 +1,19 @@
 const bcrypt = require('bcryptjs');
+
 const { User } = require('../../models/user');
 const { ConflictError, ValidationError } = require('../../errors');
 
-const SALT_LENGTH = 10;
+const { SALT_LENGTH = 10 } = process.env;
 
 async function createUser(req, res, next) {
   try {
     const { email, password, name } = req.body;
     const passwordHash = await bcrypt.hash(password, SALT_LENGTH);
-
     let user = await User.create({
       email,
       password: passwordHash,
       name,
     });
-
     user = user.toObject();
     delete user.password;
     res.status(201).send(user);
@@ -27,9 +26,8 @@ async function createUser(req, res, next) {
       next(new ConflictError('Пользователь с таким email уже существует'));
       return;
     }
-
     next(err);
   }
 }
-
+module.exports = { createUser };
 module.exports = { createUser };
