118aaa80817b5f24eca9bb8f020f1c9bf378a76f
lugvictoria <victory.senty@yandex.ru>
2023-07-09 15:21:12 +0300

    добавить validation для регистрации

--- routes/index.js
+++ routes/index.js
@@ -1,21 +1,21 @@
 const express = require('express');
-// const { celebrate, Joi } = require('celebrate');
+
 const { movies } = require('./movies');
 const { users } = require('./users');
 const { NotFoundError } = require('../errors');
 const { auth } = require('../middlewares/auth');
 const { createUser, login } = require('../controllers/users');
+const { userInfoValidator } = require('../utils/validators');
 
 const routes = express.Router();
 
 routes.all('*', express.json());
 
-routes.post('/signup', createUser);
+routes.post('/signup', userInfoValidator, createUser);
 
 routes.post('/signin', login);
 
 routes.use('/users', auth, users);
-
 routes.use('/movies', auth, movies);
 routes.all('*', (req, res, next) => {
   next(new NotFoundError('Неверный адрес запроса'));
