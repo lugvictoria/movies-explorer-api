e3961637b90d62e11e489040ecc481e9aaf2bd86
lugvictoria <victory.senty@yandex.ru>
2023-07-09 18:34:15 +0300

    переименование валидаторов

--- routes/index.js
+++ routes/index.js
@@ -4,18 +4,17 @@ const { users } = require('./users');
 const { NotFoundError } = require('../errors');
 const { auth } = require('../middlewares/auth');
 const { createUser, login } = require('../controllers/users');
-const { userInfoValidator, loginValidator } = require('../utils/validators');
+const { createUserValidator, loginValidator } = require('../utils/validators');
 
 const routes = express.Router();
 
 routes.all('*', express.json());
 
-routes.post('/signup', userInfoValidator, createUser);
+routes.post('/signup', createUserValidator, createUser);
 routes.post('/signin', loginValidator, login);
 
 routes.use('/users', auth, users);
 routes.use('/movies', auth, movies);
-
 routes.all('*', (req, res, next) => {
   next(new NotFoundError('Неверный адрес запроса'));
 });
