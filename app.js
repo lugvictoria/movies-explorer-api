c7ad4757c540901eb1720ac71612913e40124436
lugvictoria <victory.senty@yandex.ru>
2023-07-09 18:50:06 +0300

    protection with helmet

--- app.js
+++ app.js
@@ -3,11 +3,12 @@ require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const { errors } = require('celebrate');
+const helmet = require('helmet');
+
 // modules
 const { routes } = require('./routes');
 const { errorHandler } = require('./middlewares/errorHandler');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
-
 // params
 const {
   PORT = 3000,
@@ -27,13 +28,13 @@ mongoose
 
 // middlewares
 app.use(requestLogger);
+app.use(helmet());
 app.use(routes);
 
 // error handlers
 app.use(errorLogger);
 app.use(errors());
 app.use(errorHandler);
-
 // app starting
 app.listen(PORT, () => {
   console.log(`App started on port ${PORT}...`);
